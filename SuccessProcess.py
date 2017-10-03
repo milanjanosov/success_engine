8e89b6f2ec96311670f7e607fd747f4292cd90ab
janosovmilan <janosovm@gmail.com>
2017-10-03 14:54:37 +0200

    rand2.0

--- SuccessProcess.py
+++ SuccessProcess.py
@@ -198,7 +198,7 @@ def process_simple_career_trajectories(normalized, randomized):
 
 
 
-                gyurika = MultipleImpactCareerTrajectory(filename, 'Data/' + field.title() + '/' + field + '-' + label + '-simple-careers/' + filename, combined_factors, randomized)
+                gyurika = MultipleImpactCareerTrajectory(filename, 'Data/' + field.title() + '/' + field + '-' + label + '-simple-careers/' + filename, combined_factors)
                 multi_impacts += gyurika.getImpactValues()
 
 
