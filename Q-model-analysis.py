20543f3d60a8d4b1045bcd7d9f27f76dc0e949cb
janosovmilan <janosovm@gmail.com>
2017-10-09 16:18:23 +0200

    impact plots

--- Q-model-analysis.py
+++ Q-model-analysis.py
@@ -1,7 +1,7 @@
 import os
 import sys
-import matplotlib
-matplotlib.use('Agg')
+#import matplotlib
+#matplotlib.use('Agg')
 import seaborn
 import numpy as np
 
@@ -53,30 +53,42 @@ def fitPowerLaw(rand, ax, label):
     x_rand, p_rand = getDistribution(rand)
     
     
-    # fit and plot the powerlaw   
-    results = powerlaw.Fit(rand, xmin = min(x_rand))
-    fit = powerlaw.Fit(rand)
-    alpha = fit.power_law.alpha
-    xmin  = fit.power_law.xmin
-    sk_results_pow = stats.kstest(results.pdf()[1], 'powerlaw', [alpha, xmin]) 
-       
-    results.power_law.plot_pdf(color='r', ax = ax,  linestyle='-', linewidth = 3, label = '$\\alpha$= ' + str(round(alpha,2)) + ', $x_{min}$=' + str(round(xmin,2)) + '\n$D$='+str(round(sk_results_pow[0], 2))+ ', $p$='+str(round(sk_results_pow[1],2)) )     
-
-    
-            
     # fit and plot the lognormal
     print 'Fitting lognormal...'
     #param = stats.lognorm.fit(rand)
 
     #pdf_fitted = stats.lognorm.pdf(x_rand, param[0], loc=param[1], scale=param[2])#
     #mu =  np.log(param[2])
+
     #sigma = param[0]
 
+
+
+
     counts, bins, bars = ax.hist(rand, normed = True, bins = 10 ** np.linspace(np.log10(min(x_rand)), np.log10(max(x_rand)), 15), log=True,alpha=0.0)#,   histtype='step', linewidth = 0)
-    #sk_results_norm = stats.kstest(counts, 'lognorm', param)   
-    ax.plot((bins[1:] + bins[:-1])/2, counts, 's-', color = 'royalblue',alpha = 0.5, markersize = 12, linewidth = 2)
+
+    ax.plot((bins[1:] + bins[:-1])/2, counts, 's-', color = 'royalblue', alpha = 0.5, markersize = 12, linewidth = 2)
     #ax.plot(x_rand,pdf_fitted,'r-', label = '$\\mu$=' + str(round(mu,2)) + ' $\\sigma$=' + str(round(sigma, 2)) + '\n$D$='+str(round(sk_results_norm[0], 2))+ ', $p$='+str(round(sk_results_norm[1],2)) )
 
+    
+    
+    
+    
+    # fit and plot the powerlaw   
+    results = powerlaw.Fit(rand, xmin = min(x_rand), fit_method = 'KS')
+    #fit = powerlaw.Fit(rand)
+    alpha = results.power_law.alpha
+    xmin  = results.power_law.xmin
+  
+    D = results.power_law.KS()
+
+ 
+    results.power_law.plot_pdf(color='r', ax = ax,  linestyle='-', linewidth=3, label ='$\\alpha$= ' + str(round(alpha,2)) + ', $x_{min}$=' + str(round(xmin,2)) + '\n$D$='+str(round(D, 2)
+    ))     
+
+    
+            
+    
     ax.set_ylim([ min(counts), 1.1])
     ax.set_xlim([ min(x_rand),  max(bins)])
 
@@ -86,7 +98,7 @@ def fitPowerLaw(rand, ax, label):
      
      
      
-    return alpha, xmin, sk_results_pow[0], sk_results_pow[1]#, mu, sigma, sk_results_norm[0], sk_results_norm[1]
+    return alpha, xmin, D
 
 
 
@@ -102,11 +114,14 @@ def fitSkewedNormal(rand, ax, label):
     x_rand, p_rand = getDistribution(rand)
     pdf_fitted = stats.skewnorm.pdf(x_rand,  param[0], loc=param[1], scale=param[2])
      
+    
+     
     mean = stats.skewnorm.mean( param[0], loc=param[1], scale=param[2])
+    maxx = x_rand[pdf_fitted.tolist().index(max(pdf_fitted))]
     
     counts, bins, bars = ax.hist(rand, normed = True, bins = np.linspace(min(x_rand), max(x_rand), 25), alpha=0.4)
-    sk_results_norm = stats.kstest(counts, 'skewnorm', param)       
-    ax.plot(x_rand,pdf_fitted,'r-', linewidth = 3, label = '$\\mu$=' + str(round(mean, 2)) + '\n$D$='+str(round(sk_results_norm[0], 2))+ ', $p$='+str(round(sk_results_norm[1],2)))
+    sk_results_norm = stats.kstest(np.asarray(pdf_fitted), lambda x: stats.skewnorm.cdf(x_rand, param[0], loc=param[1], scale=param[2]))   # stats.ks_2samp(np.cumsum(p_rand), np.cumsu
+    ax.plot(x_rand,pdf_fitted,'r-', linewidth = 3, label = '$\\mu$=' + str(round(mean, 2)) + ', $\\mu^{*}$=' + str(maxx) + '\n$D$='+str(round(sk_results_norm[0], 2))+ ', $p$='+str(round(sk_results_norm[1],2)))
 
     return mean, sk_results_norm[0], sk_results_norm[1]
 
@@ -117,16 +132,13 @@ def fitSkewedNormal(rand, ax, label):
 def get_imapct_distr():             
             
 
-
-    ''' THIS CONFIRMS THAT THE IMPACT MEASURES OF INTEREST FOLLOW LOGNORMAL NATURE '''
-    ''' THEREFORE THE IMPACT DISTR COULD BE THE RESULT OF A MULTIPLICATIVE PROCESS '''
-     
+ 
     dir9 = 'ProcessedDataCombined/9_impact_distributions_fit'
     if not os.path.exists(dir9):
         os.makedirs(dir9)
                     
     out_pow = open(dir9 + '/' + 'impact_distribution_power_fits.dat', 'w')
-    out_pow.write('norm\tdomain\tmeasure\talpha\txmin\tD\tp\n')
+    out_pow.write('norm\tdomain\tmeasure\talpha\txmin\tD\n')
     
     out_norm = open(dir9 + '/' + 'impact_distribution_normal_fits.dat', 'w')
     out_norm.write('norm\tdomain\tmeasure\tmu\tD\tp\n')
@@ -186,6 +198,7 @@ def get_imapct_distr():
             rating_criit_fit = fitPowerLaw(critic_review,       ax[1,0], 'imdb ' + label + ' (critic reviews)')          
             rating_user_fit  = fitPowerLaw(user_review,         ax[1,1], 'imdb ' + label + ' (user reviews)')
             
+
              
             out_pow.write(mode_  + '\t' + label + '\t' + 'rating_cnt'     + '\t' + '\t'.join([str(t) for t in rating_cnt_fit])   + '\n')
             out_pow.write(mode_  + '\t' + label + '\t' + 'critic_reviews' + '\t' + '\t'.join([str(t) for t in rating_criit_fit]) + '\n')
@@ -206,12 +219,14 @@ def get_imapct_distr():
 
                 num_mus  = str(int(round(len(os.listdir('Data/Music/music-'+ genre +'-simple-careers'))/1000.0))) + 'k'
                 file_music = FOLDER + '/1_impact_distributions/music_rating_counts_dist_' + genre + '.dat'
-                rating_counts = np.asarray([round(float(line.strip())) for line in open(file_music)])    
+                rating_counts = np.asarray([float(line.strip()) for line in open(file_music)])    
+
+                
+
 
                 rating_cnt_fit   = fitPowerLaw(rating_counts,   ax[1,2], 'electronic music (rating counts)')                
                 out_pow.write(mode_ + '\t' + label + '\t' + 'rating_cnt'     + '\t' + '\t'.join([str(t) for t in rating_cnt_fit])   + '\n')
-            
-            
+                        
            
             align_plot(ax)
             plt.savefig('Figs/fitted_impact_distros_' + label+ mode + '_full.png')
