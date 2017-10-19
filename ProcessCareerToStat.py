50c636a7c22613bb4d5aa022643521dfed03bad7
janosovmilan <janosovm@gmail.com>
2017-10-20 00:23:00 +0200

    --

--- ProcessCareerToStat.py
+++ ProcessCareerToStat.py
@@ -216,7 +216,7 @@ def process_simple_career_trajectories(args):
 
           
 
-        for filename in files[0:100]:
+        for filename in files:
         
                       
             ijk += 1
