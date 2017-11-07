f091b46f095925251d433fc3b2a4a3c904a2c369
janosovmilan <janosovm@gmail.com>
2017-11-07 09:52:12 +0100

    GetSample

--- GetSampleOfTheProcessedData.py
+++ GetSampleOfTheProcessedData.py
@@ -12,73 +12,38 @@ p = 0.05
 folders = os.listdir('ProcessedData')
 for folder in folders:
 
+
+    subfolders = os.listdir('ProcessedData/'+folder)
     
     
-    if not os.path.exists('ProcessedDataSample/' + folder):
-        os.makedirs('ProcessedDataSample/' + folder)
+    for subfolder in subfolders:
 
-    files = os.listdir('ProcessedData/' + folder) 
-    for filename in files:
-        #if 'book' in filename:
-        #    p  = p/10.0
-        #else:
-        #    p = 0.05
+        folder_o   = 'ProcessedData/' + folder
+        filefolder_o = folder_o + '/' + subfolder
+        
+        folder_t   = 'ProcessedDataSample/' + folder + '_Sample'
+        filefolder_t = folder_t + '/' + subfolder
+        
+        if not os.path.exists(filefolder_t):
+            os.makedirs(filefolder_t)
 
 
-        f = open('ProcessedDataSample/' + folder + '/' + filename, 'w')
-        for line in open('ProcessedData/' + folder + '/' + filename):            
-            if random.random() < p:
-                f.write(line)                   
-        f.close()    
+        #print filefolder_o, filefolder_t
+    
+    
+        files = os.listdir(filefolder_o) 
+        for filename in files:
+            if 'book' in filename:
+                p  = p/10.0
+            else:
+                p = 0.05
+            
+            f = open(filefolder_t + '/' + filename, 'w')
+            for line in open(filefolder_o + '/' + filename):            
+                if random.random() < p:
+                    f.write(line)                   
+            f.close()    
          
   
-
-
-p = 0.05          
-    
-folders = os.listdir('ProcessedDataNormalized')
-for folder in folders:
-    
-    if not os.path.exists('ProcessedDataNormalizedSample/' + folder):
-        os.makedirs('ProcessedDataNormalizedSample/' + folder)
-
-    files = os.listdir('ProcessedDataNormalized/' + folder)  
-    for filename in files:
-
-        #if 'book' in filename:
-        #    p = p/10.0
-        #else:
-        #    p = 0.05
-
-        f = open('ProcessedDataNormalizedSample/' + folder + '/' + filename, 'w')     
-        for line in open('ProcessedDataNormalized/' + folder + '/' + filename):            
-            if random.random() < p:
-                f.write(line)                   
-        f.close()    
-                  
-
-
-
-p = 0.05
-
-folders = os.listdir('ProcessedDataNormalizedRandomized')
-for folder in folders:
-    
-    if not os.path.exists('ProcessedDataNormalizedRandomizedSample/' + folder):
-        os.makedirs('ProcessedDataNormalizedRandomizedSample/' + folder)
-
-    files = os.listdir('ProcessedDataNormalizedRandomized/' + folder)  
-    for filename in files:
-
-
-        #if 'book' in filename:
-        #    p  = p/10.0
-        #else:
-        #    p = 0.05
-
-        f = open('ProcessedDataNormalizedRandomizedSample/' + folder + '/' + filename, 'w')     
-        for line in open('ProcessedDataNormalizedRandomized/' + folder + '/' + filename):            
-            if random.random() < p:
-                f.write(line)                   
-        f.close()    
+       
        
