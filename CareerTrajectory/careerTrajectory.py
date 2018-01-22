c377d4b437614c001943fc044ec92a86af9dd391
milan <janosovm@gmail.com>
2018-01-22 21:09:28 +0100

    career time limit

--- CareerTrajectory/careerTrajectory.py
+++ CareerTrajectory/careerTrajectory.py
@@ -124,39 +124,41 @@ class SimpleCareerTrajectory:
                 #if 2 == 2:
                     year    = int(float(fields[1]))
                     impact  = float(fields[impactm + 2])
+
+                    if year > 1910 and year < 2016:
                     
-                    if impact > 0 and yearIsOK(year, date_of_birth, date_of_death) and cango:
-                        if 'no' not in normalize:
+                        if impact > 0 and yearIsOK(year, date_of_birth, date_of_death) and cango:
+                            if 'no' not in normalize:
 
-                            impact = impact/norm_factors[year]
-
-                            '''if 'yearly_avg' in normalize: 
                                 impact = impact/norm_factors[year]
 
-
-
-   
-                            elif 'years_all' in normalize: 
-                                impact = impact# * norm_factors
-
-
-                                #print norm_factors
-
-                            elif 'field_avg' in normalize: 
-                                impact = impact / norm_factors
-                                #print norm_factors
+                                '''if 'yearly_avg' in normalize: 
+                                    impact = impact/norm_factors[year]
 
 
 
+       
+                                elif 'years_all' in normalize: 
+                                    impact = impact# * norm_factors
 
-                            elif 'fields_all' in normalize: 
-                                impact = impact * norm_factors
-                                #print norm_factors
-                            '''
 
-                            
-                        if impact > 0:
-                            events.append((product, year, impact))
+                                    #print norm_factors
+
+                                elif 'field_avg' in normalize: 
+                                    impact = impact / norm_factors
+                                    #print norm_factors
+
+
+
+
+                                elif 'fields_all' in normalize: 
+                                    impact = impact * norm_factors
+                                    #print norm_factors
+                                '''
+
+                                
+                            if impact > 0:
+                                events.append((product, year, impact))
                 except:  
                     pass
 
