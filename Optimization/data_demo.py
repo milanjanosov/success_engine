4abbfafbf988cc0f4e82f911c74673edeb7d0926
milanjanosov <janosovm@gmail.com>
2018-04-13 18:23:11 +0200

    opt

--- Optimization/data_demo.py
+++ Optimization/data_demo.py
@@ -73,10 +73,10 @@ def rosen(x, *args):
  
 
 
-def get_I():
+def get_I(field):
 
 
-    files = os.listdir('DataSample/Film/film-art-director-simple-careers')
+    files = os.listdir('DataSample/Film/film-' + field + '-simple-careers')
 
     I = []
     N = []
@@ -85,7 +85,7 @@ def get_I():
 
         c_i = []
 
-        for line in gzip.open('DataSample/Film/film-art-director-simple-careers/' + filename):
+        for line in gzip.open('DataSample/Film/film-' + field + '-simple-careers/' + filename):
             if 'rating_count' not in line:
                 try:
                     ccc = float(line.strip().split('\t')[3])
@@ -105,60 +105,6 @@ def get_I():
 
 
 
-def stage1(I, N):
-
- 
-    ''' optimizer '''
-
-    for mu_Q0 in np.arange(0.5,8,1):
-
-        for mu_p0 in np.arange(0.5,8,1):
-           
-            x0  = np.array([mu_p0, mu_Q0])#, 1.0, 6.5, 1.0])
-            res = minimize(rosen, x0,  args=([I, N, 0] , ),  method='Nelder-Mead', options={'xtol': 0.1,'disp': True, 'maxiter' : 10})
-
-
-
-
-
-
-
-def stage2(I, N):
-
-
-
-    ''' input params '''
-  
-    mu = []
-
-    for line in open('out.dat'):
-
-        fields = [float(fff) for fff in line.strip().split('\t') if is_number(fff)]
-
-        if len(fields) == 4:
-            
-            if fields[3] < 0.2:
-            
-                mu.append(( fields[1], fields[2]))
-
-
-
-    ''' optimizer '''
-
-    for mu_p, mu_Q in mu:
-
-
-        x0 = np.array([mu_p, mu_Q])#, 1.0, 6.5, 1.0])
-        res = minimize(rosen, x0,  args=([I, N, 0] , ),  method='Nelder-Mead', options={'xtol': 1e-1, 'disp': True, 'maxiter' : 20})
-
-
-
-
-
-
-
-
-
 
 def rosen2(x, *args):
 
@@ -167,65 +113,54 @@ def rosen2(x, *args):
     I   = args[0][0]
     N   = args[0][1]
     ijk = args[0][2]
+
+    if x[2] > 0 and x[3] > 0:
     
-    Qs = [np.exp(pp) for pp in list(np.random.normal(x[0],  x[2], len(N)))]
-    ps = [np.exp(pp) for pp in list(np.random.normal(x[1],  x[3], len(I)))]
+        Qs = [np.exp(pp) for pp in list(np.random.normal(x[0],  x[2], len(N)))]
+        ps = [np.exp(pp) for pp in list(np.random.normal(x[1],  x[3], len(I)))]
 
-    
-    Isynt = []
+        
+        Isynt = []
 
-    for index, n in enumerate(N):
-        for i in range(n):
-            Q = Qs[index]
-            p = ps[0]
-            ps.remove(p)
-            impact = Q * p
-            Isynt.append(impact)
+        for index, n in enumerate(N):
+            for i in range(n):
+                Q = Qs[index]
+                p = ps[0]
+                ps.remove(p)
+                impact = Q * p
+                Isynt.append(impact)
 
-            
-    #S = stats.ks_2samp(np.cumsum(I), np.cumsum(Isynt))[0]
-    S = dist(I, Isynt)
+                
+        S = stats.ks_2samp(np.cumsum(I), np.cumsum(Isynt))[0]
+        #S = dist(I, Isynt)
 
-    #if S < 1.0: 
-    #if S < 0.02:
-
-    fout = open('fasz/opt_res_out2', 'a')
-    fout.write( str(x[0]) + '\t' + str(x[1]) + '\t' + str(x[2]) + '\t' + str(x[3]) + '\t' + str(S) + '\n')
-    fout.close()
+        #if S < 1.0: 
+        #if S < 0.02:
 
 
-    return S
+        fout = open(filename, 'a')
+        fout.write( str(x[0]) + '\t' + str(x[1]) + '\t' + str(x[2]) + '\t' + str(x[3]) + '\t' + str(S) + '\n')
+        fout.close()
 
 
-
-
-'''
-
-mu_N 2.1 	2.13491126007
-mu_Q 5.2 	5.2178907034
-mu_p 2.5 	2.49316451468
-sigma_N 1.0 	0.947459198111
-sigma_Q 3.0 	2.94723652079
-sigma_p 1.0 	1.00710004916
-sigma_NQ 0.05 	0.12297532998
-sigma_pQ 0.02 	0.00382356487239
-sigma_pN 0.02 	0.0211967298998
+        return S
 
 
 
-
-'''
 
 
 def stage3(*args):
 
 
-    inputs = args[0]
-    I      = args[1]
-    N      = args[2]
+    inputs   = args[0]
+    I        = args[1]
+    N        = args[2]
+    filename = args[3]
 
 
    
+
+
 
 
     ''' optimizer '''
@@ -233,7 +168,9 @@ def stage3(*args):
           
         x0 = np.array([ mu_Q0, mu_p0, sigma_Q0, sigma_p0])#, 1.0, 6.5, 1.0])
     
-        res = minimize(rosen2, x0,  args=([I, N, 0] , ),  method='Nelder-Mead', options={'xtol': 1e-1, 'disp': True, 'maxiter' : 10})
+        #print mu_Q0, mu_p0, sigma_Q0, sigma_p0
+
+        res = minimize(rosen2, x0,  args=([I, N, 0] , ),  method='Nelder-Mead', options={'xtol': 1e-1, 'disp': True, 'maxiter' : 20})
 
 
    
@@ -244,12 +181,14 @@ def stage3(*args):
 if __name__ == '__main__':  
 
 
-    I, N = get_I()
+
 
 
     if sys.argv[1] == 'stage1':
+        I, N = get_I('art-director')
         stage1(I, N)
     elif sys.argv[1] == 'stage2':
+        I, N = get_I('art-director')
         stage2(I, N)
     elif sys.argv[1] == 'stage3':
 
@@ -257,62 +196,108 @@ if __name__ == '__main__':
         if not os.path.exists(folder):
             os.makedirs(folder)
 
-        fout = open('fasz/opt_res_out2', 'w')
-        fout.close()
 
 
-
-
-
-        inputs = []
-
-        for mu_Q0 in np.arange(0.3,8,1):
-
-            for mu_p0 in np.arange(0.3,8,1):
-
-                for sigma_Q0 in np.arange(0.3,8,1):
-
-                    for sigma_p0 in np.arange(0.3,8,1):
-
-                        inputs.append((mu_Q0, mu_p0, sigma_Q0, sigma_p0))
-
-
-        num_threads = 40
-        inputs_chunks = chunkIt(inputs, num_threads)
-        Pros = []
-
-        for i in range(num_threads):
-            p = Process(target = stage3, args = (inputs_chunks[i], I, N, ))
-            Pros.append(p)
-            p.start()
-           
-        for t in Pros:
-            t.join()
-
-
-        
-
-        results = []
-
-        for line in open('fasz/opt_res_out2'):
+        for i in range(10):
     
-            value = float(line.strip().split('\t')[-1])
 
-            results.append(( value, line))
+            fields = ['director', 'producer', 'writer', 'composer', 'art-director']
 
-
-
-        sorted_results = sorted(results, key=lambda tup: tup[0])
-
-        f = open('fasz/opt_res_sorted.dat', 'w')
-
-        for v, k in sorted_results[0:10000]:
-            f.write(k)
-
-        f.close()
+            for field in fields:
 
 
+                I, N  = get_I(field)
 
+                mu_Q_min = 0.1
+                mu_Q_max = 6.1
+                mu_p_min = 0.1
+                mu_p_max = 6.1
+
+                sigma_Q_min = 0.01
+                sigma_Q_max = 0.6
+                sigma_p_min = 0.01
+                sigma_p_max = 0.6
+
+                nmu_Q     = (mu_Q_max    - mu_Q_min)    / 15.0
+                nmu_p     = (mu_p_max    - mu_p_min)    / 15.0
+                nsigma_Q  = (sigma_Q_max - sigma_Q_min) / 15.0
+                nsigma_p  = (sigma_p_max - sigma_p_min) / 15.0
+
+            
+                string = '_'.join([str(aa) for aa in [mu_Q_min, mu_Q_max, mu_p_min, mu_p_max, sigma_Q_min, sigma_Q_max, sigma_p_min, sigma_p_max]])
+
+
+                filename = 'fasz/opt_res_out_' + field + '_' + string
+
+                lfiles = len([fff.split('--')[0] for fff in os.listdir('fasz') if field in fff])
+
+                filename = filename + '--' + str(lfiles/2)
+                
+                fout = open(filename, 'w')
+                fout.close()
+
+
+                #print nmu_Q, nmu_p
+
+
+                
+
+
+                inputs = []
+
+                #print np.arange( sigma_Q_min, sigma_Q_max, nsigma_Q )
+
+                for mu_Q0 in np.arange( mu_Q_min, mu_Q_max, nmu_Q ):
+
+                    for mu_p0 in np.arange( mu_p_min, mu_p_max, nmu_p ):
+
+                        for sigma_Q0 in np.arange( sigma_Q_min, sigma_Q_max, nsigma_Q):
+
+
+
+                            for sigma_p0 in np.arange( sigma_p_min, sigma_p_max, nsigma_p ):
+
+
+                                inputs.append((mu_Q0, mu_p0, sigma_Q0, sigma_p0))
+
+
+                 
+
+
+                num_threads = 4
+                inputs_chunks = chunkIt(inputs, num_threads)
+                Pros = []
+
+                for i in range(num_threads):
+                    p = Process(target = stage3, args = (inputs_chunks[i], I, N, filename, ))
+                    Pros.append(p)
+                    p.start()
+                   
+                for t in Pros:
+                    t.join()
+
+                
+                
+
+                results = []
+
+                for line in open( filename):
+            
+                    value = float(line.strip().split('\t')[-1])
+
+                    results.append(( value, line))
+
+
+
+                sorted_results = sorted(results, key=lambda tup: tup[0])
+
+                f = open( filename + '_sorted', 'w')
+
+                for v, k in sorted_results[0:10000]:
+                    f.write(k)
+
+                f.close()
+       
 
 
 
