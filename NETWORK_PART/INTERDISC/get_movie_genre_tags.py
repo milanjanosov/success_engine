12b0a06ca7b2ed3e8c409db17ccfe0cf74ad1880
milan <janosovm@gmail.com>
2018-06-24 12:49:40 +0100

    get_genre_tags

---
+++ NETWORK_PART/INTERDISC/get_movie_genre_tags.py
@@ -0,0 +1,27 @@
+mport pandas as pd
+import os
+
+root  = 'movie_profiles/'
+files = [root + fn for fn in os.listdir(root)]
+
+movies_genres = {}
+
+
+for fn in files:
+    
+    df = pd.DataFrame.from_csv(fn)
+    
+    
+    for movie, genres in df['genre_tags'].to_dict().items():
+
+        if movie not in movies_genres:
+            movies_genres[movie]  = genres.split('\t')
+        else:
+            movies_genres[movie] += genres.split('\t')
+    
+    
+fout = open('movies_genre_tags.dat', 'w')    
+for ind, (movie, genres) in enumerate(movies_genres.items()):
+    #if ind == 10: break
+    fout.write(movie + '\t' + '\t'.join(genres) + '\n')
+fout.close()
