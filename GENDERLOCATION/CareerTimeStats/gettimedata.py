a3c14a4c226e2066d01d2906abe7c53d8d6aa8eb
milanjanosov <janosovm@gmail.com>
2018-09-04 09:33:17 +0200

    up

--- GENDERLOCATION/CareerTimeStats/gettimedata.py
+++ GENDERLOCATION/CareerTimeStats/gettimedata.py
@@ -5,20 +5,20 @@ import gzip
 
 
 data_folder  = '../../Data'     
-input_fields = [#(data_folder + '/Film/film-art-director-simple-careers',   'film',       'art-director'),   
-              #  (data_folder + '/Music/music-pop-simple-careers',          'music',      'pop'),
-              #  (data_folder + '/Music/music-electro-simple-careers',      'music',      'electro'),
-              #  (data_folder + '/Music/music-classical-simple-careers',    'music',      'classical'),
-                (data_folder + '/Music/music-folk-simple-careers',         'music',      'folk')]#,
-              #  (data_folder + '/Music/music-funk-simple-careers',         'music',      'funk'),
-             #  (data_folder + '/Music/music-jazz-simple-careers',         'music',      'jazz'),
-              #  (data_folder + '/Music/music-hiphop-simple-careers',       'music',      'hiphop'),                   		
-             #   (data_folder + '/Music/music-rock-simple-careers',         'music',      'rock'),  
-             #   (data_folder + '/Film/film-director-simple-careers',       'film',       'director'),
-             #   (data_folder + '/Film/film-producer-simple-careers',       'film',       'producer'),   
-              #  (data_folder + '/Film/film-writer-simple-careers',         'film',       'writer'),   
-              #  (data_folder + '/Film/film-composer-simple-careers',       'film',       'composer'),   
-             #   (data_folder + '/Book/book-authors-simple-careers',        'book',       'authors') ]
+input_fields = [(data_folder + '/Film/film-art-director-simple-careers',   'film',       'art-director'),   
+                (data_folder + '/Music/music-pop-simple-careers',          'music',      'pop'),
+                (data_folder + '/Music/music-electro-simple-careers',      'music',      'electro'),
+                (data_folder + '/Music/music-classical-simple-careers',    'music',      'classical'),
+                (data_folder + '/Music/music-folk-simple-careers',         'music',      'folk'),
+                (data_folder + '/Music/music-funk-simple-careers',         'music',      'funk'),
+                (data_folder + '/Music/music-jazz-simple-careers',         'music',      'jazz'),
+                (data_folder + '/Music/music-hiphop-simple-careers',       'music',      'hiphop'),                   		
+                (data_folder + '/Music/music-rock-simple-careers',         'music',      'rock'),  
+                (data_folder + '/Film/film-director-simple-careers',       'film',       'director'),
+                (data_folder + '/Film/film-producer-simple-careers',       'film',       'producer'),   
+                (data_folder + '/Film/film-writer-simple-careers',         'film',       'writer'),   
+                (data_folder + '/Film/film-composer-simple-careers',       'film',       'composer'),   
+                (data_folder + '/Book/book-authors-simple-careers',        'book',       'authors') ]
 
 
 
@@ -33,6 +33,8 @@ for (folder, field, label)  in input_fields:
 rootfolder =  '../../QMODELNEW/pQData'
 careers    =  ['director-10', 'art_director-20', 'composer-10', 'writer-10', 'producer-10'] + ['electro-80', 'rock-80', 'pop-80', 'jazz-80', 'classical-80', 'funk-80', 'folk-80', 'hiphop-80']+  ['authors-50']
 
+
+careers    =  [ 'funk-80']
 
 
 users = {}
@@ -55,6 +57,7 @@ if not os.path.exists(folderout):
 
 ''' =========================================================== '''
 '''                  map musician names to ids                  '''
+
 
 classical = {}
 for line in open('Music/artist_ids_discogs_classical.dat'):
