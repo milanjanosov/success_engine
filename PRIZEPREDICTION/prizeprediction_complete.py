b43cbefe871c970ace5119428249e014c0f4b111
milan <janosovm@gmail.com>
2018-10-01 09:10:22 +0200

    up

--- PRIZEPREDICTION/prizeprediction_complete.py
+++ PRIZEPREDICTION/prizeprediction_complete.py
@@ -10,7 +10,7 @@ import os
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.cross_validation import cross_val_score
+from sklearn.model_selection import cross_val_score
 from sklearn import preprocessing
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
