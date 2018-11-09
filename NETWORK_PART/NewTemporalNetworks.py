e03070166ed575df1f37eb1b553bc5b6f2cb7154
milan <janosovm@gmail.com>
2018-11-09 17:40:59 +0100

    up

--- NETWORK_PART/NewTemporalNetworks.py
+++ NETWORK_PART/NewTemporalNetworks.py
@@ -167,8 +167,147 @@ def create_cumulative_careers():
 
 
 
+'''     GET THE DIRECTORS COLLABORATION CAREERS    '''
+
+
+def jaccard(a, b):
+    c = a.intersection(b)
+    return float(len(c)) / (len(a) + len(b) - len(c))
+
+
+def get_networks():
+
+
+    directors        = set([f.split('_')[0] for f in os.listdir('simple-careers/film-director-simple-careers')])
+    movies_years     = {} 
+    start_years      = {}
+    directors_movies = {}
+
+    for line in open('ALL_movies_years.dat'):
+        year, movie  = line.strip().split('\t')
+        movies_years[movie] = year   
+
+    for line in open('ALL_directors_starting_year.dat'):
+        name, year = line.strip().split('\t')
+        start_years[name] = int(year)
+
+    directors_s = directors.intersection(set(start_years.keys()))
+    directors   = list(directors.intersection(set(start_years.keys())))
+    edges_cnt   = {}
+    edges_jacc  = {}
+
+
+    director_movies = {}
+
+
+    dirrrrs = ['nm0000184', 'nm0000245']
+
+    for ind, dddd in enumerate(directors):
+
+        #if dddd in dirrrrs:
+        print 'Parse careers   ', ind
+
+        for line in open('NEWTemporal/2_directors_cumulative_careers/' + dddd + '.dat'):
+            
+            fields = line.strip().split('\t')
+            year   = int(fields[0])
+            movies = set(fields[1:])
+    
+            if dddd not in director_movies: director_movies[dddd] = {}
+            director_movies[dddd][year] = movies
+
+
+
+    for jind, line in enumerate(open('ALL_movies_casts.dat')):
+    
+       # if jind == 1000: break
+  
+        fields  = line.strip().split('\t')
+        movie   = fields[0]
+        cast    = sorted(fields[1:])
+        cast_s  = set(cast)
+        cast_d  = set(cast_s.intersection(directors_s))
+        cast_l  = list(set(cast_s.intersection(directors_s)))
+        year    = get_year(movies_years[movie])
+
+
+        #print jind, '\t703216\t', len(cast_s) 
+   
+
+
+        for ind, director1 in enumerate(cast_l):
+
+            if director1 in dirrrrs:
+
+                start1 = start_years[director1]
+
+                for director2 in cast_l[ind+1:]:
+
+                    #if director2 in dirrrrs:
+
+                    start2 = start_years[director2]
+
+                    if year >= start1 and year >= start2:
+
+                        if year not in edges_cnt:  edges_cnt[year]  = {}
+                        if year not in edges_jacc: edges_jacc[year] = {}
+
+        
+
+                        movies1  = director_movies[director1][year]
+                        movies2  = director_movies[director2][year]
+                        jaccardv = jaccard(movies1, movies2)
+                        count    = len(movies1.intersection(movies2))
+                        edge     = '--'.join(sorted([director1, director2]))
+
+                        edges_cnt[year][edge]  = count
+                        edges_jacc[year][edge] = jaccardv
+
+                        print edge, year, count, jaccardv
+
+
+
+    folderout = 'NEWTemporal/3_edgelists/'
+
+    gout = open('NEWTemporal/3_edgelists/networks_size_over_time.dat', 'w')
+    
+    for year, edges in edges_jacc.items():
+
+        gout.write(str(year) + '\t' + str(len(edges)) + '\n')
+
+        edgefolder = folderout + str(year) + '/'
+        if not os.path.exists(edgefolder): os.makedirs(edgefolder)
+
+        fout = open(edgefolder + 'edge_list_jaccard.dat', 'w')
+        for edge, weight in edges.items():
+            fout.write(edge + '\t' + str(weight) + '\n')
+        fout.close()
+
+    gout.close()
+
+
+    for year, edges in edges_cnt.items():
+
+        edgefolder = folderout + str(year) + '/'
+        if not os.path.exists(edgefolder): os.makedirs(edgefolder)
+
+        fout = open(edgefolder + 'edge_list_count.dat', 'w')
+        for edge, weight in edges.items():
+            fout.write(edge + '\t' + str(weight) + '\n')
+        fout.close()
+
+
 
 
 #get_career_start()
 #get_directors_all_contributed_movies()
-create_cumulative_careers()
+#create_cumulative_careers()
+
+get_networks()
+
+
+
+
+
+
+
