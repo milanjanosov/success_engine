053366a5cc15f43042a847a7268b43da91a32ee5
milan <janosovm@gmail.com>
2018-11-23 14:01:33 +0100

    optimizer sh

--- robotsim/atlasz/evolution/test/optimizer.sh
+++ robotsim/atlasz/evolution/test/optimizer.sh
@@ -5,4 +5,4 @@ echo "First arg: $2"
 echo "First arg: $3"
 echo "First arg: $4"
 
-screen -S $1 bash -c "python2.7 test_evolution.py -m  $2 -min $3 -max $4 "
+screen -S $1 bash -c "source /opt/virtualenv-python2.7/bin/activate; python2.7 test_evolution.py -m  $2 -min $3 -max $4 "
