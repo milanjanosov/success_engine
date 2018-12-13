4f5541ba7ceb6473306fdfedea1fe9eae9cf6c9d
milan <janosovm@gmail.com>
2018-12-13 20:18:15 +0100

    up

--- robotsim/atlasz/evolution/test/collect_rescaleres.py
+++ robotsim/atlasz/evolution/test/collect_rescaleres.py
@@ -3,7 +3,7 @@ import sys
 
 
 
-def collect():
+def update():
 
 
     folders = [fff for fff in os.listdir('Results_linrescaled')]
@@ -90,6 +90,95 @@ def collect():
 
     STSOUT.close()
 
+   
+
+def collect():
+
+
+    folders = [fff for fff in os.listdir('Results_linrescaled')]
+
+
+
+    if os.path.exists('opt_status_report_linrescaled.dat'):
+
+        PREVSTAT          = {}
+        PREVSTAT['RAW']   = {}
+        PREVSTAT['CLEAN'] = {}
+
+        for line in open('opt_status_report_linrescaled.dat'):
+            fields = line.strip().split('\t')
+        
+            fieldname = fields[1], fields[1].split('_')[-1]
+
+            PREVSTAT[fields[0]][fieldname] = int(fields[2].replace('.0', ''))
+
+
+      
+    STSOUT = open('opt_status_report_linrescaled.dat', 'w')
+
+
+
+    outfolder = 'MLESUCCESS_RES_linrescaled'
+    if not os.path.exists(outfolder):
+        os.makedirs(outfolder)
+
+    harminc = len('Results_linrescaled/sci_theoretical_computer_science')
+
+    for folder in folders:
+
+
+
+        outfile = open(outfolder + '/Genetic_results_' + folder.split('_', 1)[-1] + '.dat', 'w')
+
+
+        outfile.write('\t'.join(['maxfitness', 'mu_N', 'mu_p', 'mu_Q', 'sigma_N', 'sigma_Q', 'sigma_p', 'sigma_pQ', 'sigma_pN', 'sigma_QN']) + '\n')
+
+        folder = 'Results_linrescaled/' + folder
+        runs = [folder + '/' + run for run in os.listdir(folder) if '.dat' not in run]
+
+        fieldname = folder.replace('sci_', '')
+
+    
+        if len(runs) == 1000 or len(runs) == 500:
+            print ('DONE:   \t',  folder, '    ', ''.join((harminc - len(folder))*[' ']),len(runs), '\t')
+
+        else:
+            print ('Running:\t',  folder, '    ', ''.join((harminc - len(folder))*[' ']),len(runs), '\t')
+        
+
+
+            
+        STSOUT.write('RAW\t' + folder.replace('sci_', '') + '\t' + str(len(runs)) + '\n')
+
+        if len(runs) > 0:
+
+            for run in runs:
+
+                if len(os.listdir(run)) > 0:
+
+                    lastgen = max([ int(fff.split('_')[1].replace('.dat', '')) for fff in  os.listdir(run) ])
+                    resfile = run + '/Generation_' + str(lastgen) + '.dat'
+
+                    best = ''
+                    maxf = ''   
+
+                    for line in open(resfile):
+                        if 'max' in line:
+                            maxf = line.strip().split('\t')[1]
+                        if 'best' in line:
+                            best = line.strip().split('\t', 1)[1]
+
+                    outfile.write( maxf + '\t' + best + '\n')
+
+        outfile.close()
+            
+
+
+    print ('\n\n')
+           
+
+    STSOUT.close()
+
     '''files = [f for f in os.listdir('MLESUCCESS_RES_linrescaled') ]
 
 
@@ -148,6 +237,9 @@ def collect():
 
 
 
+
+
+
 def export():
 
 
@@ -196,6 +288,7 @@ def export():
 
 
 if   sys.argv[1] == 'collect': collect()
+elif sys.argv[1] == 'update':  update()
 elif sys.argv[1] == 'export':  export()
 
 
