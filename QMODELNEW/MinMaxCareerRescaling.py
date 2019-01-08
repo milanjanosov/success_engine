dc0f499aadbc6cd81b1db1ad4d780bcb70a32abd
milan <janosovm@gmail.com>
2019-01-08 13:06:29 +0100

    up

--- QMODELNEW/MinMaxCareerRescaling.py
+++ QMODELNEW/MinMaxCareerRescaling.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import os
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from scipy import stats
